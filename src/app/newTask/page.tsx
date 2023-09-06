46e6ccce43f92c255ac1a2136d842311a36d8662
saddathasan <saddathasan94@gmail.com>
2023-09-06 09:33:17 +0600

    added the component, form and new page

--- src/app/newTask/page.tsx
+++ src/app/newTask/page.tsx
@@ -1,3 +1,20 @@
-export default function about () {
-    return <h2>this page will be built later</h2>
+export default function NewTodo () {
+    return (
+        <>
+        <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">new</h1>
+    
+    </header>
+
+        <form className="flex gap-2 flex-col">
+            <input
+                type="text"
+                name="title"
+                className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-200"
+             />
+
+             
+        </form>
+</>
+    )
 }
